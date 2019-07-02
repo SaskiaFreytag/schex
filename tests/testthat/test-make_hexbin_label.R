f1d13ebf865573e94b92ee6021f8d639b52f11b0
SaskiaFreytag <saskia.freytag@perkins.uwa.edu.au>
2019-07-02 14:41:10 +0800

    more tests again

--- tests/testthat/test-make_hexbin_label.R
+++ tests/testthat/test-make_hexbin_label.R
@@ -2,3 +2,12 @@ test_that("correct class", {
   expect_equal(class({pbmc_small <- make_hexbin(pbmc_small, 10, dimension_reduction = "PCA");
     make_hexbin_label(pbmc_small, col="RNA_snn_res.1")}), "data.frame")
 })
+
+test_that("error no hexbin", {
+  expect_error(make_hexbin_label(pbmc_small, col="RNA_snn_res.1"))
+})
+
+test_that("error wrong col", {
+  pbmc_small <- make_hexbin(pbmc_small, 10, dimension_reduction = "PCA")
+  expect_error(make_hexbin_label(pbmc_small, col="RNA_snn_res.2"))
+})
