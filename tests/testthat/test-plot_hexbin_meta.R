08fa0cf0e41e4288bcce49ea8914d7f586bc1402
SaskiaFreytag <saskia.freytag@perkins.uwa.edu.au>
2019-07-02 14:41:10 +0800

    more tests again

--- tests/testthat/test-plot_hexbin_meta.R
+++ tests/testthat/test-plot_hexbin_meta.R
@@ -1,6 +1,11 @@
-test_that("correct class", {
+test_that("correct class prop", {
   expect_equal(class({pbmc_small <- make_hexbin(pbmc_small, 10, dimension_reduction = "PCA");
   plot_hexbin_meta(pbmc_small, col="RNA_snn_res.1", action="prop", no=1)})[2], "ggplot")
+})
+
+test_that("correct class majority", {
+  expect_equal(class({pbmc_small <- make_hexbin(pbmc_small, 10, dimension_reduction = "PCA");
+  plot_hexbin_meta(pbmc_small, col="RNA_snn_res.1", action="majority")})[2], "ggplot")
 })
 
 test_that("error no hexbin", {
@@ -18,7 +23,7 @@ test_that("error plot_hexbin", {
   expect_error(schex:::.plot_hexbin(drhex, colour_by="Cluster_majority"))
 })
 
-test_that("correct plot_hexbin", {
+test_that("correct plot_hexbin majority", {
   pbmc_small <- make_hexbin(pbmc_small, 10, dimension_reduction = "PCA")
   drhex <- pbmc_small@misc$hexbin$hexbin.matrix
   drhex <- as_tibble(drhex)
@@ -26,3 +31,22 @@ test_that("correct plot_hexbin", {
   expect_equal(class(schex:::.plot_hexbin(drhex, colour_by="lab_majority"))[2],
                "ggplot")
 })
+
+test_that("correct plot_hexbin prop", {
+  pbmc_small <- make_hexbin(pbmc_small, 10, dimension_reduction = "PCA")
+  drhex <- pbmc_small@misc$hexbin$hexbin.matrix
+  drhex <- as_tibble(drhex)
+  drhex$lab_prop_2 <- c(rep(c("A", "B"), dim(drhex)[1]/2))
+  expect_equal(class(schex:::.plot_hexbin(drhex, colour_by="lab_prop_2"))[2],
+               "ggplot")
+})
+
+test_that("correct plot_hexbin numeric", {
+  pbmc_small <- make_hexbin(pbmc_small, 10, dimension_reduction = "PCA")
+  drhex <- pbmc_small@misc$hexbin$hexbin.matrix
+  drhex <- as_tibble(drhex)
+  drhex$lab_mean <- c(rep(c(1, 2), dim(drhex)[1]/2))
+  expect_equal(class(schex:::.plot_hexbin(drhex, colour_by="lab_mean"))[2],
+               "ggplot")
+})
+
