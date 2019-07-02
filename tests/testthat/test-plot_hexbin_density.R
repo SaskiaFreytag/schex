fc4dfd3c6c745c35bda40df65139c7bab9b33a58
SaskiaFreytag <saskia.freytag@perkins.uwa.edu.au>
2019-07-02 13:33:32 +0800

    so many more tests

--- tests/testthat/test-plot_hexbin_density.R
+++ tests/testthat/test-plot_hexbin_density.R
@@ -2,3 +2,7 @@ test_that("correct class", {
   expect_equal(class({pbmc_small <- make_hexbin(pbmc_small, 10, dimension_reduction = "PCA");
   plot_hexbin_density(pbmc_small)})[2], "ggplot")
 })
+
+test_that("error no hexbin", {
+  expect_error(plot_hexbin_density(pbmc_small))
+})
--- tests/testthat/test-utility_functions.R
+++ tests/testthat/test-utility_functions.R
@@ -45,4 +45,61 @@ test_that("make_hexbin_function median", {
   cID <- c(1,1,1,2,2,2,3,3,3)
   expect_equal(schex:::.make_hexbin_function(x, action, cID),
                c(2,2,2))
-  })
+})
+
+test_that("make_hexbin_function prop_0", {
+  x <- rep(c(0,0,1), 3)
+  action <- "prop_0"
+  cID <- c(1,1,1,2,2,2,3,3,3)
+  expect_equal(schex:::.make_hexbin_function(x, action, cID),
+               c(1/3,1/3,1/3))
+})
+
+test_that("make_hexbin_function error action", {
+  x <- rep(c(2,2,1), 3)
+  action <- "error"
+  cID <- c(1,1,1,2,2,2,3,3,3)
+  expect_error(schex:::.make_hexbin_function(x, action, cID))
+})
+
+test_that("make_hexbin_function error prop", {
+  x <- rep(c(2,2,1), 3)
+  action <- "prop"
+  cID <- c(1,1,1,2,2,2,3,3,3)
+  expect_error(schex:::.make_hexbin_function(x, action, cID))
+})
+
+test_that("make_hexbin_function error majority", {
+  x <- rep(c(2,2,1), 3)
+  action <- "majority"
+  cID <- c(1,1,1,2,2,2,3,3,3)
+  expect_error(schex:::.make_hexbin_function(x, action, cID))
+})
+
+test_that("make_hexbin_function error mode", {
+  x <- as.factor(rep(c("A", "A", "B"), 3))
+  action <- "mode"
+  cID <- c(1,1,1,2,2,2,3,3,3)
+  expect_error(schex:::.make_hexbin_function(x, action, cID))
+})
+
+test_that("make_hexbin_function error mean", {
+  x <- as.factor(rep(c("A", "A", "B"), 3))
+  action <- "mean"
+  cID <- c(1,1,1,2,2,2,3,3,3)
+  expect_error(schex:::.make_hexbin_function(x, action, cID))
+})
+
+test_that("make_hexbin_function error median", {
+  x <- as.factor(rep(c("A", "A", "B"), 3))
+  action <- "median"
+  cID <- c(1,1,1,2,2,2,3,3,3)
+  expect_error(schex:::.make_hexbin_function(x, action, cID))
+})
+
+test_that("make_hexbin_function error prop_0", {
+  x <- as.factor(rep(c("A", "A", "B"), 3))
+  action <- "prop_0"
+  cID <- c(1,1,1,2,2,2,3,3,3)
+  expect_error(schex:::.make_hexbin_function(x, action, cID))
+})
