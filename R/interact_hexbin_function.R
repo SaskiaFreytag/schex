275b5c85b7131959b232d1f339b949d1e785b14f
SaskiaFreytag <saskia.freytag@perkins.uwa.edu.au>
2019-10-08 12:15:09 +0800

    reduce duplicated code

---
+++ R/interact_hexbin_function.R
@@ -0,0 +1,43 @@
+.interact_hexbin_function<- function(first_x, second_x, interact, cID) {
+  
+  if(interact=="corr_spearman"){
+    
+    func_if <- !(is.numeric(first_x)|is.numeric(second_x))
+    
+    if (func_if) {
+      stop(paste0("Features need to be numeric."))
+      
+    } else {
+      
+      res_first <- tapply(first_x, cID, FUN = function(z) z)
+      res_second <- tapply(second_x, cID, FUN = function(z) z)
+      
+      res <- unlist(lapply(seq_len(length(res_first)), function(x)
+        cor(res_first[[x]], res_second[[x]], method="spearman")))
+      
+      return(res)
+    }
+  }
+  
+  if(interact=="mi"){
+    
+    func_if <- !(is.numeric(first_x)|is.numeric(second_x))
+    
+    if (func_if) {
+      stop(paste0("Features need to be numeric."))
+      
+    } else {
+      
+      res_first <- tapply(first_x, cID, FUN = function(z) z)
+      res_second <- tapply(second_x, cID, FUN = function(z) z)
+      
+      res <- lapply(seq_len(length(res_first)), function(x)
+        rbind(res_first[[x]], res_second[[x]]))
+      
+      res <- unlist(lapply(res, function(x)
+        mi.plugin(x)))
+      
+      return(res)
+    }
+  }
+}  
--- R/schex.R
+++ R/schex.R
@@ -5,4 +5,14 @@
 #'
 #' @docType package
 #' @name schex
+#' @details Please see the help pages listed below:
+#'  \itemize{
+#'     \item{\code{\link[schex]{make_hexbin}}}
+#'     \item{\code{\link[schex]{plot_hexbin_gene}}}
+#'     \item{\code{\link[schex]{plot_hexbin_meta}}}
+#'  }
+#'  Also see the vignette for more usage examples:
+#'  \code{browseVignettes("schex")}
+#'  Please report issues and suggest improvements at Github:
+#'  \link{https://github.com/SaskiaFreytag/schex}
 NULL
