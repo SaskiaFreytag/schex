a166f7492c47da2d01e3c7a4594d23d656012521
Miles Smith <miles-smith@omrf.org>
2019-11-07 16:23:27 -0600

    Add lower and upper cutoff arguments to plot_hexbin_feature
    Former-commit-id: b28ef23e36839ea92336a281bb1bef840622ab66
    Former-commit-id: 374a24984d0362c4beccec4e9053fe1ccf59f923
    Former-commit-id: d0bdf06e0763dbdc4ed1f3014a2552319ff01512

--- R/make_hexbin.R
+++ R/make_hexbin.R
@@ -38,88 +38,87 @@
 #' library(TENxPBMCData)
 #' library(scater)
 #' tenx_pbmc3k <- TENxPBMCData(dataset = "pbmc3k")
-#' rm_ind <- calculateAverage(tenx_pbmc3k)<0.1
-#' tenx_pbmc3k <- tenx_pbmc3k[!rm_ind,]
+#' rm_ind <- calculateAverage(tenx_pbmc3k) < 0.1
+#' tenx_pbmc3k <- tenx_pbmc3k[!rm_ind, ]
 #' tenx_pbmc3k <- normalize(tenx_pbmc3k)
 #' tenx_pbmc3k <- runPCA(tenx_pbmc3k)
-#' tenx_pbmc3k <- make_hexbin( tenx_pbmc3k, 80, dimension_reduction = "PCA")
+#' tenx_pbmc3k <- make_hexbin(tenx_pbmc3k, 80, dimension_reduction = "PCA")
 #' }
 setGeneric("make_hexbin", function(sce, nbins = 80,
-    dimension_reduction = "UMAP",
-    use_dims=c(1,2))
-    standardGeneric("make_hexbin"))
+                                   dimension_reduction = "UMAP",
+                                   use_dims = c(1, 2)) {
+  standardGeneric("make_hexbin")
+})
 
 #' @export
 #' @describeIn make_hexbin Bivariate binning of SingleCellExperiment
 #'   into hexagon cells.
 setMethod("make_hexbin", "SingleCellExperiment", function(sce,
-    nbins = 80,
-    dimension_reduction = "UMAP",
-    use_dims=c(1,2)) {
-  
-    if(!is.element(dimension_reduction, reducedDimNames(sce))) {
-        stop("Specify existing dimension reduction.")
-    }
-  
-    dr <- reducedDim(sce, dimension_reduction)
-  
-    res <- .make_hexbin_helper(dr, nbins, use_dims)
-    sce@metadata$hexbin<- res
-  
-    return(sce)
+                                                          nbins = 80,
+                                                          dimension_reduction = "UMAP",
+                                                          use_dims = c(1, 2)) {
+  if (!is.element(dimension_reduction, reducedDimNames(sce))) {
+    stop("Specify existing dimension reduction.")
+  }
+
+  dr <- reducedDim(sce, dimension_reduction)
+
+  res <- .make_hexbin_helper(dr, nbins, use_dims)
+  sce@metadata$hexbin <- res
+
+  return(sce)
 })
 
 #' @export
 #' @describeIn make_hexbin Bivariate binning of Seurat
 #'   into hexagon cells.
 setMethod("make_hexbin", "Seurat", function(
-    sce,
-    nbins = 80,
-    dimension_reduction = "UMAP",
-    use_dims=c(1,2)) {
-  
-    if(!is.element(tolower(dimension_reduction), names(sce@reductions))) {
-        stop("Specify existing dimension reduction.")
-    }
-  
-    func <- paste0("dr <- sce@reductions$", tolower(dimension_reduction),
-        "@cell.embeddings")
-  
-    eval(parse(text = func))
-  
-    res <- .make_hexbin_helper(dr, nbins, use_dims)
-    sce@misc$hexbin <- res
-  
-    return(sce)
-  
+                                            sce,
+                                            nbins = 80,
+                                            dimension_reduction = "UMAP",
+                                            use_dims = c(1, 2)) {
+  if (!is.element(tolower(dimension_reduction), names(sce@reductions))) {
+    stop("Specify existing dimension reduction.")
+  }
+
+  func <- paste0(
+    "dr <- sce@reductions$", tolower(dimension_reduction),
+    "@cell.embeddings"
+  )
+
+  eval(parse(text = func))
+
+  res <- .make_hexbin_helper(dr, nbins, use_dims)
+  sce@misc$hexbin <- res
+
+  return(sce)
 })
 
 .make_hexbin_helper <- function(dr, nbins = 80, use_dims) {
-  
-    if(dim(dr)[2] < max(use_dims)) {
-        stop("Please specify use_dims that are calculated.")
-    }
-  
-    xbnds <- range(c(dr[, use_dims[1]]))
-    ybnds <- range(c(dr[, use_dims[2]]))
-  
-    drhex <- hexbin(dr[, use_dims[1]],
-        dr[, use_dims[2]],
-        nbins,
-        xbnds = xbnds,
-        ybnds = ybnds,
-        IDs = TRUE
-    )
-    cID <- drhex@cID
-    drhex <- cbind(as.numeric(hcell2xy(drhex)$x),
-        as.numeric(hcell2xy(drhex)$y),
-        as.numeric(drhex@count))
-  
-    colnames(drhex) <- c("x", "y", "number_of_cells")
-  
-    res <- list(cID=cID, hexbin.matrix=drhex)
-  
-    return(res)
+  if (dim(dr)[2] < max(use_dims)) {
+    stop("Please specify use_dims that are calculated.")
+  }
+
+  xbnds <- range(c(dr[, use_dims[1]]))
+  ybnds <- range(c(dr[, use_dims[2]]))
+
+  drhex <- hexbin(dr[, use_dims[1]],
+    dr[, use_dims[2]],
+    nbins,
+    xbnds = xbnds,
+    ybnds = ybnds,
+    IDs = TRUE
+  )
+  cID <- drhex@cID
+  drhex <- cbind(
+    as.numeric(hcell2xy(drhex)$x),
+    as.numeric(hcell2xy(drhex)$y),
+    as.numeric(drhex@count)
+  )
+
+  colnames(drhex) <- c("x", "y", "number_of_cells")
+
+  res <- list(cID = cID, hexbin.matrix = drhex)
+
+  return(res)
 }
-
-
--- R/plot_hexbin_meta.R
+++ R/plot_hexbin_meta.R
@@ -23,8 +23,8 @@
 #'    \code{prop}, \code{prop_0}, \code{mode}, \code{mean} and \code{median}:
 #'
 #'    \describe{
-#'       \item{\code{majority}}{Returns the value of the majority of 
-#'       observations in the bin. The associated meta data column needs to be 
+#'       \item{\code{majority}}{Returns the value of the majority of
+#'       observations in the bin. The associated meta data column needs to be
 #'       a factor or character.}
 #'       \item{\code{prop}}{Returns the proportion of each level or unique
 #'       character in the bin. The associated meta data column needs to be a
@@ -51,141 +51,147 @@
 #' library(Seurat)
 #' data("pbmc_small")
 #' pbmc_small <- make_hexbin(pbmc_small, 10, dimension_reduction = "PCA")
-#' plot_hexbin_meta(pbmc_small, col="RNA_snn_res.1", action="prop", no=1)
-#' plot_hexbin_meta(pbmc_small, col="RNA_snn_res.1", action="prop", no=2)
+#' plot_hexbin_meta(pbmc_small, col = "RNA_snn_res.1", action = "prop", no = 1)
+#' plot_hexbin_meta(pbmc_small, col = "RNA_snn_res.1", action = "prop", no = 2)
 #' # For SingleCellExperiment object
 #' \dontrun{
 #' library(TENxPBMCData)
 #' library(scater)
 #' tenx_pbmc3k <- TENxPBMCData(dataset = "pbmc3k")
-#' rm_ind <- calculateAverage(tenx_pbmc3k)<0.1
-#' tenx_pbmc3k <- tenx_pbmc3k[-rm_ind,]
-#' colData(tenx_pbmc3k) <- cbind(colData(tenx_pbmc3k),
-#'      perCellQCMetrics(tenx_pbmc3k))
+#' rm_ind <- calculateAverage(tenx_pbmc3k) < 0.1
+#' tenx_pbmc3k <- tenx_pbmc3k[-rm_ind, ]
+#' colData(tenx_pbmc3k) <- cbind(
+#'   colData(tenx_pbmc3k),
+#'   perCellQCMetrics(tenx_pbmc3k)
+#' )
 #' tenx_pbmc3k <- normalize(tenx_pbmc3k)
 #' tenx_pbmc3k <- runPCA(tenx_pbmc3k)
-#' tenx_pbmc3k <- make_hexbin( tenx_pbmc3k, 20, dimension_reduction = "PCA")
-#' plot_hexbin_meta(tenx_pbmc3k, col="total", action="median")
+#' tenx_pbmc3k <- make_hexbin(tenx_pbmc3k, 20, dimension_reduction = "PCA")
+#' plot_hexbin_meta(tenx_pbmc3k, col = "total", action = "median")
 #' }
 setGeneric("plot_hexbin_meta", function(sce,
-    col,
-    action,
-    no = 1,
-    colors=NULL,
-    title=NULL,
-    xlab=NULL,
-    ylab=NULL) standardGeneric("plot_hexbin_meta"))
+                                        col,
+                                        action,
+                                        no = 1,
+                                        colors = NULL,
+                                        title = NULL,
+                                        xlab = NULL,
+                                        ylab = NULL) {
+  standardGeneric("plot_hexbin_meta")
+})
 
 #' @export
 #' @describeIn plot_hexbin_meta  Plot of meta data into hexagon cell for
 #'   SingleCellExperiment object.
 setMethod("plot_hexbin_meta", "SingleCellExperiment", function(sce,
-    col,
-    action,
-    no = 1,
-    colors=NULL,
-    title=NULL,
-    xlab=NULL,
-    ylab=NULL) {
-  
-    out <- sce@metadata$hexbin[[2]]
-    cID <- sce@metadata$hexbin[[1]]
-  
-    if (any(!col %in% colnames(colData(sce)))) {
-        stop("Column cannot be found in colData(sce).")
-    }
-  
-    name_s <- paste0("sce$", col)
-    func <- paste0("x <- ", name_s)
-  
-    eval(parse(text = func))
-  
-    .plot_hexbin_meta_helper(x, out, cID, col, action, no, title, xlab, ylab,
-        colors)
+                                                               col,
+                                                               action,
+                                                               no = 1,
+                                                               colors = NULL,
+                                                               title = NULL,
+                                                               xlab = NULL,
+                                                               ylab = NULL) {
+  out <- sce@metadata$hexbin[[2]]
+  cID <- sce@metadata$hexbin[[1]]
+
+  if (any(!col %in% colnames(colData(sce)))) {
+    stop("Column cannot be found in colData(sce).")
+  }
+
+  name_s <- paste0("sce$", col)
+  func <- paste0("x <- ", name_s)
+
+  eval(parse(text = func))
+
+  .plot_hexbin_meta_helper(
+    x, out, cID, col, action, no, title, xlab, ylab,
+    colors
+  )
 })
 
 #' @export
 #' @describeIn plot_hexbin_meta  Plot of meta data into hexagon cell for
 #'   Seurat object.
 setMethod("plot_hexbin_meta", "Seurat", function(sce,
-    col,
-    action,
-    no = 1,
-    colors=NULL,
-    title=NULL,
-    xlab=NULL,
-    ylab=NULL) {
-  
-    out <- sce@misc$hexbin[[2]]
-    cID <- sce@misc$hexbin[[1]]
-  
-    if (any(!col %in% colnames(sce@meta.data))) {
-        stop("Column cannot be found in slot(sce, 'meta.data').")
-    }
-  
-    name_s <- paste0("sce$", col)
-    func <- paste0("x <- ", name_s)
-  
-    eval(parse(text = func))
-  
-    .plot_hexbin_meta_helper(x, out, cID, col, action, no, title, xlab, ylab,
-          colors)
-  
+                                                 col,
+                                                 action,
+                                                 no = 1,
+                                                 colors = NULL,
+                                                 title = NULL,
+                                                 xlab = NULL,
+                                                 ylab = NULL) {
+  out <- sce@misc$hexbin[[2]]
+  cID <- sce@misc$hexbin[[1]]
+
+  if (any(!col %in% colnames(sce@meta.data))) {
+    stop("Column cannot be found in slot(sce, 'meta.data').")
+  }
+
+  name_s <- paste0("sce$", col)
+  func <- paste0("x <- ", name_s)
+
+  eval(parse(text = func))
+
+  .plot_hexbin_meta_helper(
+    x, out, cID, col, action, no, title, xlab, ylab,
+    colors
+  )
 })
 
 .plot_hexbin_meta_helper <- function(x, out, cID, col, action, no, title,
-      xlab, ylab, colors) {
-  
-    if(is.null(out)){
-        stop("Compute hexbin representation before plotting.")
-    }
-  
-    hh <- .make_hexbin_function(x, action, cID)
-    out <- as_tibble(out)
-  
-    if (action == "prop"|action=="majority") {
-        if(action == "prop"){
-            col_hh <- .make_hexbin_colnames(x, col)
-            func1 <- paste0("out$", col_hh, " <- hh[,", 
-                seq(1,length(col_hh),1),"]")
-            for(i in seq_len(length(func1))){
-                eval(parse(text=func1[i]))
-            }
-        }
-        if(action == "majority"){
-            col_hh <-paste0(col, "_", action)
-            if(is.factor(x)){
-                func1 <- paste0("out$", col_hh, " <- factor(hh, levels=",
-                    "levels(x))")
-            } else {
-                func1 <- paste0("out$", col_hh, " <- hh")
-            }
-            eval(parse(text=func1))
-        }
-    } else {
-        col_hh <-paste0(col, "_", action)
-        func1 <- paste0("out$", col_hh, " <- hh")
-        eval(parse(text=func1))
-    }
-  
-    if(action!="prop"){
-    
-        if(action=="majority"){
-      
-            .plot_hexbin(out, colour_by=col_hh, colors=colors,
-                title=title, xlab=xlab, ylab=ylab)
-      
-        } else {
-      
-            .plot_hexbin(out, colour_by=col_hh, colors=NULL,
-                title=title, xlab=xlab, ylab=ylab)
-        }
-    
-    } else {
-    
-        .plot_hexbin(out, colour_by=col_hh[no], colors=NULL,
-            title=title, xlab=xlab, ylab=ylab)
-    
-    }
-}
+                                     xlab, ylab, colors) {
+  if (is.null(out)) {
+    stop("Compute hexbin representation before plotting.")
+  }
 
+  hh <- .make_hexbin_function(x, action, cID)
+  out <- as_tibble(out)
+
+  if (action == "prop" | action == "majority") {
+    if (action == "prop") {
+      col_hh <- .make_hexbin_colnames(x, col)
+      func1 <- paste0(
+        "out$", col_hh, " <- hh[,",
+        seq(1, length(col_hh), 1), "]"
+      )
+      for (i in seq_len(length(func1))) {
+        eval(parse(text = func1[i]))
+      }
+    }
+    if (action == "majority") {
+      col_hh <- paste0(col, "_", action)
+      if (is.factor(x)) {
+        func1 <- paste0(
+          "out$", col_hh, " <- factor(hh, levels=",
+          "levels(x))"
+        )
+      } else {
+        func1 <- paste0("out$", col_hh, " <- hh")
+      }
+      eval(parse(text = func1))
+    }
+  } else {
+    col_hh <- paste0(col, "_", action)
+    func1 <- paste0("out$", col_hh, " <- hh")
+    eval(parse(text = func1))
+  }
+
+  if (action != "prop") {
+    if (action == "majority") {
+      .plot_hexbin(out,
+        colour_by = col_hh, colors = colors,
+        title = title, xlab = xlab, ylab = ylab
+      )
+    } else {
+      .plot_hexbin(out,
+        colour_by = col_hh, colors = NULL,
+        title = title, xlab = xlab, ylab = ylab
+      )
+    }
+  } else {
+    .plot_hexbin(out,
+      colour_by = col_hh[no], colors = NULL,
+      title = title, xlab = xlab, ylab = ylab
+    )
+  }
+}
