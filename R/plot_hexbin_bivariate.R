103b32fb0a3ff9ae5d974f175446fa79e9179ab5
cindyTAT <wanchengt@student.unimelb.edu.au>
2023-12-17 16:20:44 +1100

    modified plot_hexbin_bivariate

--- R/plot_hexbin_bivariate.R
+++ R/plot_hexbin_bivariate.R
@@ -44,6 +44,10 @@
 #' library(TENxPBMCData)
 #' library(scater)
 #' tenx_pbmc3k <- TENxPBMCData(dataset = "pbmc3k")
+#' rm_ind <- calculateAverage(tenx_pbmc3k) < 0.1
+#' tenx_pbmc3k <- tenx_pbmc3k[!rm_ind, ]
+#' tenx_pbmc3k <- logNormCounts(tenx_pbmc3k)
+#' tenx_pbmc3k <- runPCA(tenx_pbmc3k)
 #' tenx_pbmc3k <- make_hexbin(tenx_pbmc3k, 10, dimension_reduction = "PCA")
 #' plot_hexbin_bivariate(tenx_pbmc3k, type="RNA", feature="CD3D")
 #' plot_hexbin_bivariate(tenx_pbmc3k, type="RNA", feature="CD3D", fan=TRUE)
