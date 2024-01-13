9cf38c09611f9211cb236eaee2d3fb6d65855a9d
cindyTAT <wanchengt@student.unimelb.edu.au>
2024-01-14 09:57:43 +1100

    Removed Seurat from feature_plus

--- R/plot_hexbin_feature_plus.R
+++ R/plot_hexbin_feature_plus.R
@@ -1,18 +1,15 @@
 #' Plot of gene expression and meta data of single cell data in
 #' bivariate hexagon cells.
 #'
-#' @param sce A \code{\link[SingleCellExperiment]{SingleCellExperiment}}
-#'   or \code{\link[Seurat]{Seurat-class}} object.
+#' @param sce A \code{\link[SingleCellExperiment]{SingleCellExperiment}}.
 #' @param col A string referring to the name of one column in the meta data of
 #'    sce by which to colour the hexagons.
 #' @param mod A string referring to the name of the modality used for plotting.
 #'     For RNA modality use "RNA". For other modalities use name of alternative 
-#'     object for the \code{\link[SingleCellExperiment]{SingleCellExperiment}} 
-#'     or the name of the assay for the \code{\link[Seurat]{Seurat-class}} 
+#'     object for the \code{\link[SingleCellExperiment]{SingleCellExperiment}}
 #'     object.
 #' @param type A string referring to the type of assay in the
-#'   \code{\link[SingleCellExperiment]{SingleCellExperiment}} object or the
-#'   data transformation in the \code{\link[Seurat]{Seurat-class}} object.
+#'   \code{\link[SingleCellExperiment]{SingleCellExperiment}} object.
 #' @param feature A string referring to the name of one feature.
 #' @param action A string specifying how gene expression of observations in
 #'   binned  hexagon cells are to be summarized. Possible actions are
@@ -45,11 +42,8 @@
 #'      The associated meta data column needs to be numeric.}
 #'   }
 #'   
-#'    To access the data that has been integrated in the 
-#'    \code{\link[Seurat]{Seurat-class}} object specifiy \code{mod="integrated"}.
 #'
 #' @return A \code{\link{ggplot2}{ggplot}} object.
-#' @import Seurat
 #' @import SingleCellExperiment
 #' @import ggplot2
 #' @importFrom dplyr as_tibble
@@ -58,12 +52,17 @@
 #' @export
 #'
 #' @examples
-#' #' # For Seurat object
-#' library(Seurat)
-#' data("pbmc_small")
-#' pbmc_small <- make_hexbin(pbmc_small, 10, dimension_reduction = "PCA")
-#' plot_hexbin_feature_plus(pbmc_small, col="RNA_snn_res.1", type="RNA",
-#'    feature="NRBP1", action="mean")
+#' #' # For SingleCellExperiment object
+#' library(TENxPBMCData)
+#' library(scater)
+#' tenx_pbmc3k <- TENxPBMCData(dataset = "pbmc3k")
+#' rm_ind <- calculateAverage(tenx_pbmc3k) < 0.1
+#' tenx_pbmc3k <- tenx_pbmc3k[!rm_ind, ]
+#' tenx_pbmc3k <- logNormCounts(tenx_pbmc3k)
+#' tenx_pbmc3k <- runPCA(tenx_pbmc3k)
+#' tenx_pbmc3k <- make_hexbin(tenx_pbmc3k, 10, dimension_reduction = "PCA") 
+#' plot_hexbin_feature_plus(tenx_pbmc3k, col="Sample", type="counts",
+#'    feature="ENSG00000135250", action="mean")
 plot_hexbin_feature_plus <- function(sce,
     col,
     mod="RNA",
